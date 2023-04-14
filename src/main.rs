41431e69fd2d2d304d049f61b881458473b22499
Jack Shrader <96661942+jpshrader@users.noreply.github.com>
2023-04-14 16:14:18 -0400

    I'm Rust

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,14 @@
-use serde_json::from_reader;
-use std::fs::File;
-use std::io::BufReader;
-use serde_json::Value;
-//use reqwest::get;
+use std::io;
 
 fn main() {
-    let name = "Rust with containers";
-    println!("Hello, {}!", name);
-}
+    println!("What is your name?: ");
 
-// write a function to make an api call with reqwest
-// Path: src/main.rs
-async fn make_api_call() {
-    // let url = "https://jsonplaceholder.typicode.com/todos/1";
-    // let response = get(url).await.unwrap();
-    // let json: Value = response.json().await.unwrap();
-    // println!("JSON:\n{:#?}", json);
-}
-
-
-// write a function to read from a json file
-// Path: src/main.rs
-fn read_from_json_file() {
-    let file = File::open("data.json").unwrap();
-    let reader = BufReader::new(file);
-    let json: Value = from_reader(reader).unwrap();
-    println!("JSON:\n{:#?}", json);
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(n) => {
+            println!("{n} bytes read");
+            println!("Hello {}!", input.trim());
+        }
+        Err(error) => println!("error: {error}"),
+    }
 }
